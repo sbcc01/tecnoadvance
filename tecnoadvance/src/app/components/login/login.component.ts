42793b803e6c4df5dd5fbe8515582e7acc0f22c2
SergioRuiz719 <126711109+SergioRuiz719@users.noreply.github.com>
2023-06-08 12:07:30 -0600

    Se cambio el login/registro
    se implementaron cambios en el login como en el registro.
    Procederemos a crear los servicios para que se guarden los datos del registro

--- tecnoadvance/src/app/components/login/login.component.ts
+++ tecnoadvance/src/app/components/login/login.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-
-
+import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
-  formRegister: FormGroup = new FormGroup({});
+  formRegister: FormGroup  = new FormGroup({});
+
+  selectedForm: string = 'login'; // aparesca login por defecto
+
+  selectForm(form: string) {
+    this.selectedForm = form;
+  }
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.formLogin = this.formBuilder.group({
       email: ['', Validators.required],
-      password: ['', [Validators.required]],
+      password: ['', Validators.required],
     });
 
     this.formRegister = this.formBuilder.group({
@@ -27,21 +33,19 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  saveLogin() {
+  submitLogin() {
     if (this.formLogin.valid) {
-      let formData = new FormData();
-      formData.append('email', this.formLogin.get('email')!.value);
-      formData.append('password', this.formLogin.get('password')!.value);
+      const formData = this.formLogin.value;
+      // Se debe implementar la funcion para que se lleve acabo el login
+      console.log('Ingresar:', formData);
     }
   }
 
-  saveRegister() {
+  submitRegistration() {
     if (this.formRegister.valid) {
-      let formData = new FormData();
-      formData.append('name', this.formRegister.get('name')!.value);
-      formData.append('phone', this.formRegister.get('phone')!.value);
-      formData.append('email', this.formRegister.get('email')!.value);
-      formData.append('password', this.formRegister.get('password')!.value);
+      const formData = this.formRegister.value;
+      // Codigo para que los datos se almacenen en la base de datos
+      console.log('Registrarse:', formData);
     }
   }
 }
